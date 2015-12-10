d22f82c3f20e1b9a7f34c5c1cae2941bbe03d931
Nicolas Gemsjäger <KazimirDante>
2015-12-10 14:28:27 +0100

    feature #83 - Kachelansicht

--- templates/bootstrap3/javascript/bootstrap-add.js
+++ templates/bootstrap3/javascript/bootstrap-add.js
@@ -3,3 +3,59 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
+
+function getRowSize() {
+	
+	if ($(window).width() < 768) {
+		return 1;
+	} else if ($(window).width() < 992) {
+		return 2;
+	} else if ($(window).width() < 1200) {
+		return 3;
+	} else {
+		return 3;
+	}
+}
+
+function resizeProductBoxes() {
+	var maxHeight = [];       
+	var MyClass = "";
+	var MyRow = 0;
+	$(".equalize").each(function(){
+		
+		$(this).height('auto');
+				
+		MyClass = $(this).attr('class').replace('equalize', '').trim();
+		MyRow = Math.ceil($(this).parent().attr("data-col") / getRowSize());
+	
+		if (typeof maxHeight[MyRow] == "undefined") {
+			maxHeight[MyRow] = [];
+		}
+		if (typeof maxHeight[MyRow][MyClass] == "undefined") {
+			maxHeight[MyRow][MyClass] = 0;
+		}
+		if ($(this).height() > maxHeight[MyRow][MyClass]) {
+			maxHeight[MyRow][MyClass] = $(this).height();
+		}
+		
+	});      
+
+	$(".equalize").each(function(){
+		
+		MyClass = $(this).attr('class').replace('equalize', '').trim();
+		MyRow = Math.ceil($(this).parent().attr("data-col") / getRowSize());
+		$(this).height(maxHeight[MyRow][MyClass]);
+		
+	}); 	
+}
+
+$(document).ready(function() {
+   resizeProductBoxes();		
+}); 
+
+$(window).resize(function() {
+    clearTimeout($.data(this, 'resizeTimer'));
+    $.data(this, 'resizeTimer', setTimeout(function() {
+       resizeProductBoxes();
+    }, 50));
+});

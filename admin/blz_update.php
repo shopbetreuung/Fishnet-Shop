c798745916c3da6c16aef0d1c30aea922e4c9d12
BojanYNWA <b.memic@hoqmee.net>
2017-03-09 14:26:31 +0100

    Fixed encoding of bank names for blz_update function

--- admin/blz_update.php
+++ admin/blz_update.php
@@ -122,7 +122,7 @@ require (DIR_WS_INCLUDES.'head.php');
           // and fill it with the the content from the downloaded file
           foreach ($banktransfer as $rec) {
             $sql = sprintf('insert into banktransfer_blz (blz, bankname, prz) values (%s, \'%s\', \'%s\')',
-           (int)$rec['blz'], xtc_db_input($rec['bankname']), xtc_db_input($rec['prz']));
+           (int)$rec['blz'], xtc_db_input(utf8_encode($rec['bankname'])), xtc_db_input($rec['prz']));
             xtc_db_query($sql);
             if(mysqli_affected_rows($connection) != 0) {
               $j = $j + mysqli_affected_rows($connection); // sum up affected rows

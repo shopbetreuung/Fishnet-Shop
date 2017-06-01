6b5ba956e50e616db434c03151c8d4228b8b3f89
damilakovic <d.milakovic@hoqmee.net>
2017-06-01 12:35:29 +0200

    Update phpmailer sql

---
+++ _installer/sql/update_1.8.0_to_1.9.0 .sql
@@ -0,0 +1,3 @@
+INSERT INTO configuration (configuration_id, configuration_key, configuration_value, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES ('', 'SMTP_SECURE', '---', 12, 15, NULL, NOW(), NULL, 'xtc_cfg_select_option(array(\'---\', \'ssl\', \'tls\'),');
+
+UPDATE database_version SET version = 'SH_1.9.0';

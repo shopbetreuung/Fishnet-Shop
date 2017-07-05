f4dfbba1e2003c31907fb8b1be9f13315a442c40
damilakovic <d.milakovic@hoqmee.net>
2017-07-05 15:38:25 +0200

    1.9.2 version set

--- admin/includes/application_top.php
+++ admin/includes/application_top.php
@@ -30,7 +30,7 @@
    --------------------------------------------------------------*/
 
 // DB version, used for updates (_installer)
-define('DB_VERSION', 'SH_1.9.0');
+define('DB_VERSION', 'SH_1.9.2');
 //Run Mode
 define('RUN_MODE_ADMIN',true);
 

7264cb7ba5aa7a54448cfa19299babe9e5156270
BojanYNWA <b.memic@hoqmee.net>
2017-07-06 14:18:02 +0200

    1.9.3 version set

--- admin/includes/application_top.php
+++ admin/includes/application_top.php
@@ -30,7 +30,7 @@
    --------------------------------------------------------------*/
 
 // DB version, used for updates (_installer)
-define('DB_VERSION', 'SH_1.9.2');
+define('DB_VERSION', 'SH_1.9.3');
 //Run Mode
 define('RUN_MODE_ADMIN',true);
 

9ca8ce5edc5e601723f3a218bea1394b568d2f9a
damilakovic <d.milakovic@hoqmee.net>
2017-11-06 15:49:26 +0100

    Update admin_access for invoiced_orders and inventory

--- _installer/sql/update_1.10.4_to_1.11.0.sql
+++ _installer/sql/update_1.10.4_to_1.11.0.sql
@@ -1,5 +1,8 @@
 UPDATE database_version SET version = 'SH_1.11.0';
 
 ALTER TABLE admin_access ADD inventory INT(1) NOT NULL DEFAULT '0';
+UPDATE admin_access SET inventory = '1' WHERE customers_id = '1';
+
 ALTER TABLE admin_access ADD invoiced_orders INT(1) NOT NULL DEFAULT '0';
+UPDATE admin_access SET invoiced_orders = '1' WHERE customers_id = '1';
 

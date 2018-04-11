aa1664001d057d001865e57a117011511c152feb
damilakovic <d.milakovic@hoqmee.net>
2018-04-11 09:09:30 +0200

    1.12.1 version set

--- admin/includes/application_top.php
+++ admin/includes/application_top.php
@@ -30,7 +30,7 @@
    --------------------------------------------------------------*/
 
 // DB version, used for updates (_installer)
-define('DB_VERSION', 'SH_1.12.0');
+define('DB_VERSION', 'SH_1.12.1');
 //Run Mode
 define('RUN_MODE_ADMIN',true);
 

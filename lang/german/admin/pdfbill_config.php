6faf0bc26e6097ac6dae3938597e73b225de2e93
damilakovic <d.milakovic@hoqmee.net>
2018-06-27 10:50:43 +0200

    Fixed title and button text

--- lang/german/admin/pdfbill_config.php
+++ lang/german/admin/pdfbill_config.php
@@ -25,7 +25,8 @@ $help_var_posdata = kl('
   *p_name* = Artikelname                                          <br />  
   *p_single_price* = Artikel Einzelpreis                          <br />  
   *p_price* = Preis der Position (Menge x Einzelpreis)            <br />  
-  *p_qty* = Menge                                                 <br />  
+  *p_qty* = Menge                                                 <br /> 
+  *p_single_tax* = Steuer pro Produkt                             <br />
 ');                                                              
 
 

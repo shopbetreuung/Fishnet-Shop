cfb3ba45f64010261dcd469e312e6c33164bcf51
php-schubser <d.bjelajac@hoqmee.net>
2019-01-21 10:54:49 +0100

    1.15.0 version set

--- admin/includes/version.php
+++ admin/includes/version.php
@@ -3,4 +3,4 @@
 defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.' );
 
 // Define the project version
-define('PROJECT_VERSION', 'shophelfer 1.14.1');
+define('PROJECT_VERSION', 'shophelfer 1.15.0');

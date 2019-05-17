5c868862ce823cc0cd97e34111b19e2e4ad63995
php-schubser <d.bjelajac@hoqmee.net>
2019-05-17 14:08:18 +0200

    1.16.0 version set

--- admin/includes/application_top.php
+++ admin/includes/application_top.php
@@ -30,7 +30,7 @@
    --------------------------------------------------------------*/
 
 // DB version, used for updates (_installer)
-define('DB_VERSION', 'SH_1.15.1');
+define('DB_VERSION', 'SH_1.16.0');
 //Run Mode
 define('RUN_MODE_ADMIN',true);
 
